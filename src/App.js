33f19372a90ccd270488ce068d7bbc5e052fc56b
Alejandro <alejandroaperez1994g@gmail.com>
2023-04-12 15:06:35 +0200

    feat: :sparkles: remove unused files, added react review session

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const getAllUsers = async () => {
+    const response = await fetch("http://localhost:4000/user/all")
+    const data = await response.json()
+    console.log(data);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch("http://localhost:4000/user/signup", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json"
+  
+      },
+      body: JSON.stringify(userData)
+    })
+
+    const data = await response.json();
+    console.log(data);
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1>SignUp</h1>
+      <form action="" onSubmit={(e)=>handleSubmit(e)}>
+        <input type="text" name="name" placeholder="Enter your name" value={userData.name} onChange={(e)=>handleChange(e)}/>
+        <input type="text" name="email" placeholder="Enter your email"  value={userData.email} onChange={(e)=>handleChange(e)}/>
+        <input type="text" name="password" placeholder="Enter your password" value={userData.password} onChange={(e)=>handleChange(e)}/>
+        <button type="submit">SignUp</button>
+      </form>
+
+      <button onClick={getAllUsers}>GET ALL USERS</button>
     </div>
   );
 }
